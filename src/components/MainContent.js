4d066efade45538355355513e56efd79ca69617f
Adam Bugaj <adampeter.bugaj@gmail.com>
2018-05-26 15:39:26 +0200

    Main content was updated - hide it on start

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -4,28 +4,33 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Card, CardMedia, CardText, CardActions, CardTitle } from 'material-ui/Card';
 
 const MainContent = (props) => {
-  console.log(props)
+  console.log(props.videoId)
   return (
+
     <div className="content-conatainer_maincontent">
+      {!!props.videoId &&
       <MuiThemeProvider>
-        <Card>
-          <CardMedia>
-            <iframe
-              width="250"
-              height="250"
-              src={`https://www.youtube.com/embed/${props.videoId}`}
-              frameBorder="0"
-              allow="autoplay; encrypted-media"
-              allowFullScreen>
-            </iframe>
-          </CardMedia>
-          <CardTitle title={`Nazwa: ${props.value.transactionName}`} />
-          <CardText>
-            Video description{props.searchInput}
-          </CardText>
-        </Card>
+          <Card>
+            <CardMedia>
+              <iframe
+                width="250"
+                height="250"
+                src={`https://www.youtube.com/embed/${props.videoId}`}
+                frameBorder="0"
+                allow="autoplay; encrypted-media"
+                allowFullScreen>
+              </iframe>
+            </CardMedia>
+            <CardTitle title={`Nazwa: ${props.value.transactionName}`} />
+            <CardText>
+              Video description{props.searchInput}
+            </CardText>
+          </Card>
+
       </MuiThemeProvider>
+      }
     </div>
+
   )
 }
 
